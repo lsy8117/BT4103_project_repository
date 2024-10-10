728e9ce57fea907b3e4dc849aa3dbd7eba539480
alfredyy <alfredyiow@gmail.com>
2024-10-11 00:15:48 +0800

    undo anonymizer change

--- backend/anonymizer.py
+++ backend/anonymizer.py
@@ -1,24 +1,24 @@
 from langchain_experimental.data_anonymizer import PresidioReversibleAnonymizer
 from langchain_experimental.data_anonymizer.deanonymizer_matching_strategies import combined_exact_fuzzy_matching_strategy
-from faker import Faker
-from presidio_anonymizer.entities import OperatorConfig
+# from faker import Faker
+# from presidio_anonymizer.entities import OperatorConfig
 
 analyzed_fields = ['PERSON', 'EMAIL_ADDRESS', 'PHONE_NUMBER', 'IBAN_CODE', 'CREDIT_CARD', 'CRYPTO', 'IP_ADDRESS', 'LOCATION',
                    #    'DATE_TIME',
                    'NRP', 'MEDICAL_LICENSE', 'URL', 'US_BANK_NUMBER', 'US_DRIVER_LICENSE', 'US_ITIN', 'US_PASSPORT', 'US_SSN', 'SG_NRIC_FIN', 'SG_UEN']
 
-fake = Faker()
+# fake = Faker()
 
-def fake_name(x):
-    return fake.first_name()
+# def fake_name(x):
+#     return fake.first_name()
 
-new_operators = {"PERSON": OperatorConfig("custom", {"lambda": fake_name})}
+# new_operators = {"PERSON": OperatorConfig("custom", {"lambda": fake_name})}
 
 class AnonymizerEngine:
     def __init__(self):
         # Create the anonymizer with the configuration
         self.anonymizer = PresidioReversibleAnonymizer(analyzed_fields=analyzed_fields, faker_seed=42)
-        self.anonymizer.add_operators(new_operators)
+        # self.anonymizer.add_operators(new_operators)
 
     def anonymize(self, text):
         # Anonymize the provided text with name standardization.
